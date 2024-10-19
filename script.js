a5d9239d333b77d77c0fa93bae2c8f03ecc68933
Sara Miller <qqdolphin88@gmail.com>
2024-10-19 08:16:21 -0700

    added hover to navbar items

---
+++ script.js
@@ -0,0 +1,13 @@
+function onButtonClick() {
+    alert('Button clicked!');
+}
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+    alert('button clicked!');
+  });
+button.addEventListener('mouseover', () => {
+    // change button's color
+    
+})
+
